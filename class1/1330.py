7a41e0728217c5cbea1c780d00bc9b0428723025
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-07-21 04:44:00 +0900

    백준 class1

---
+++ class1/1330.py
@@ -0,0 +1,32 @@
+# A, B = map(int, input().split())
+# if(A < B):
+#     print("<")
+# elif(A > B):
+#     print(">")
+# else:
+#     print("==")
+
+# for i in range(int(input())):
+#     for j in range(i + 1):
+#         print("*", end = "")
+#     print('')
+
+# N = int(input())
+# for i in range(1, 10):
+#     print('{} * {} = {}'.format(N, i, N * i))
+
+# A, B = map(int, input().split())
+# C = [A + B, A - B, A * B, A // B, A % B]
+# print("\n".join(map(str, C)))
+
+# import sys
+# while True:
+#     try:
+#         a, b = map(int, sys.stdin.readline().split())
+#         if(a == 0 and b == 0):
+#             break
+#         print(a + b)
+#     except:
+#         break
+
+print(ord(input()))
