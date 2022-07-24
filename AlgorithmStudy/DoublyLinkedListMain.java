476a1411bfa43edf6916f9b3ccd06125454f68fa
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-07-24 14:57:10 +0900

    DoublyLinkedList 구현

--- AlgorithmStudy/DoublyLinkedListMain.java
+++ AlgorithmStudy/DoublyLinkedListMain.java
@@ -1,35 +1,36 @@
 package AlgorithmStudy;
 
-class Node<E>{
-    Node(){
+class Node<E> {
+    Node() {
     }
-    Node(E data){
+
+    Node(E data) {
         this.data = data;
     }
+
     E data;
     Node<E> next = null;
     Node<E> prev = null;
 }
 
-class DoublyLinkedList<E>{
+class DoublyLinkedList<E> {
     private Node<E> firstNode;
     private Node<E> tailNode;
     private int size;
 
-    DoublyLinkedList(){
+    DoublyLinkedList() {
         this.firstNode = null;
         this.tailNode = null;
         this.size = 0;
     }
 
-    void add(E data){
-        if(firstNode == null){
+    void add(E data) {
+        if (firstNode == null) {
             firstNode = tailNode = new Node<E>(data);
             firstNode.next = firstNode;
             firstNode.prev = firstNode;
             size += 1;
-        }
-        else{
+        } else {
             tailNode.next = new Node<E>(data);
             tailNode.next.prev = tailNode;
             tailNode = tailNode.next;
@@ -39,28 +40,27 @@ class DoublyLinkedList<E>{
         }
     }
 
-    boolean remove(Object o){
+    boolean remove(Object o) {
         Node<E> findNode = firstNode;
-    
-        if(isEmpty()){
+
+        if (isEmpty()) {
             return false;
         }
 
-        for(int i = 0;i < size; i++, findNode = findNode.next){
-            if(o.equals(findNode.data)){
+        for (int i = 0; i < size; i++, findNode = findNode.next) {
+            if (o.equals(findNode.data)) {
                 break;
             }
         }
 
-        if(findNode.next == firstNode){
+        if (findNode.next == firstNode) {
             findNode.data = null;
             findNode.next = null;
             findNode.prev = null;
             size--;
             return true;
-        }
-        else{
-            if(findNode == firstNode){
+        } else {
+            if (findNode == firstNode) {
                 findNode.prev.next = findNode.next;
                 findNode.next.prev = findNode.prev;
                 firstNode = findNode.next;
@@ -79,66 +79,61 @@ class DoublyLinkedList<E>{
             return true;
         }
     }
-    
-    boolean contains(Object value){
+
+    boolean contains(Object value) {
         return indexOf(value) >= 0;
     }
 
-    int size(){
+    int size() {
         return size;
     }
 
-    Node<E> search(int index){
-        if(index > size / 2){
+    Node<E> search(int index) {
+        if (index > size / 2) {
             Node<E> x = tailNode;
-            for(int i = size - 1; i > index; i--){
+            for (int i = size - 1; i > index; i--) {
                 x = x.prev;
             }
             return x;
-        }
-        else{
+        } else {
             Node<E> x = firstNode;
-            for(int i = 0; i < index; i++){
+            for (int i = 0; i < index; i++) {
                 x = x.next;
             }
             return x;
         }
     }
 
-    E get(int index){
+    E get(int index) {
         return search(index).data;
     }
-    
-    E set(int index, E elements){
+
+    E set(int index, E elements) {
         Node<E> replaceNode = search(index);
         replaceNode.data = elements;
         return elements;
     }
-    
-    boolean isEmpty(){
+
+    boolean isEmpty() {
         return size == 0;
     }
 
-    // boolean equals(Object o){
-        
-    // }
-
-    int indexOf(Object o){
-        if(!isEmpty()){
+    int indexOf(Object o) {
+        if (!isEmpty()) {
             int index = 0;
-            for(Node<E> findNode = firstNode; findNode.next != firstNode; findNode = findNode.next){
-                if(o.equals(findNode.data)){
+            for (Node<E> findNode = firstNode; findNode.next != firstNode; findNode = findNode.next) {
+                if (o.equals(findNode.data)) {
                     return index;
-                } 
-                index++; 
+                }
+                index++;
             }
             return index;
         }
         return -1;
     }
 
-    void clear(){
-        for(Node<E> x = firstNode; x != null;){
+    void clear() {
+        for (Node<E> x = firstNode; x != null;) {
             Node<E> nextNode = x.next;
             x.data = null;
             x.next = null;
@@ -149,11 +144,11 @@ class DoublyLinkedList<E>{
         tailNode = null;
         size = 0;
     }
+
     void show() {
-        if(isEmpty()){
+        if (isEmpty()) {
             System.out.println("empty");
-        }
-        else{
+        } else {
             Node<E> curNode = firstNode;
             do {
                 System.out.println(curNode.data);
@@ -163,8 +158,7 @@ class DoublyLinkedList<E>{
     }
 }
 
-
-public class DoublyLinkedListMain{
+public class DoublyLinkedListMain {
     public static void main(String[] args) {
         DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
         dll.add(4);
